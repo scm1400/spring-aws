962b34b0326144d9e0adec6f3f5bc6498bc6267e
Muice <scm1400@gmail.com>
2021-09-06 18:20:16 +0900

    Mustache 화면 구성

--- src/main/java/spring/java/webapp/service/posts/PostsService.java
+++ src/main/java/spring/java/webapp/service/posts/PostsService.java
@@ -1,11 +1,15 @@
 package spring.java.webapp.service.posts;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import spring.java.webapp.domain.posts.Posts;
 import spring.java.webapp.domain.posts.PostsRepository;
+import spring.java.webapp.web.dto.PostsListsResponseDto;
 import spring.java.webapp.web.dto.PostsResponseDto;
 import spring.java.webapp.web.dto.PostsSaveRequestDto;
 import spring.java.webapp.web.dto.PostsUpdateRequestDto;
@@ -14,6 +18,7 @@ import spring.java.webapp.web.dto.PostsUpdateRequestDto;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+    
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
@@ -35,5 +40,17 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListsResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListsResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/spring/java/webapp/web/IndexController.java
+++ src/main/java/spring/java/webapp/web/IndexController.java
@@ -1,13 +1,41 @@
 package spring.java.webapp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.RequiredArgsConstructor;
+import spring.java.webapp.service.posts.PostsService;
+import spring.java.webapp.web.dto.PostsResponseDto;
+import spring.java.webapp.web.dto.PostsUpdateRequestDto;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
     
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate (@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        
+        return "posts-update";
+    }
+    
 }
